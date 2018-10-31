09071ccb68ca7215b8d2a425043ce5f09270ecf9
Anderson Chen <ander.chen00@gmail.com>
2018-10-31 19:46:21 -0300

    Make front better

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  article = Article.new
+  article.title = Faker::TheITCrowd.quote
+  article.content = Faker::Lorem.paragraph_by_chars(256, false)
+  article.save
+end
